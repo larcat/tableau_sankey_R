6eca3afae339d08c9bd466c8200d2be90f4177ec
larcat <larcatdm@gmail.com>
2018-04-06 15:22:53 -0400

    Update sankey_func.R

--- sankey_func.R
+++ sankey_func.R
@@ -9,19 +9,13 @@
 sankey_munge <- function(sank_data_lim){
 
   sank_data_lim$ID <- 1:nrow(sank_data_lim)
-
   sank_sankey <- sank_data_lim[rep(seq_len(nrow(sank_data_lim)), each=98),]
-
   sank_sankey$path <- c(0:48, 97:49)
-
   minmax <- c(rep("min", 49), rep("max", 49))
-
   sank_sankey$min_max <- minmax
-
   time <- c(rep(seq(-6, 6, .25), 2))
-
   sank_sankey$T <- time
-
+  
   return(sank_sankey)
 
 }
